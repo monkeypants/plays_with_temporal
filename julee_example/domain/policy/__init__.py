6004f5a3eb965ac8225255900719b1136728579d
Michael Nelson <absoludity@gmail.com>
2025-09-17 10:43:48 +1000

    Add DocumentPolicyValidation model.

--- julee_example/domain/policy/__init__.py
+++ julee_example/domain/policy/__init__.py
@@ -2,8 +2,14 @@ from .policy import (
     Policy,
     PolicyStatus,
 )
+from .document_policy_validation import (
+    DocumentPolicyValidation,
+    DocumentPolicyValidationStatus,
+)
 
 __all__ = [
     "Policy",
     "PolicyStatus",
+    "DocumentPolicyValidation",
+    "DocumentPolicyValidationStatus",
 ]
