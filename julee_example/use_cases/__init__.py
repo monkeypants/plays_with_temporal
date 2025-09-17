0462eacbb366e945863bc023bfd4f442710500fc
Michael Nelson <absoludity@gmail.com>
2025-09-17 16:38:25 +1000

    Add initial ValidateDocument use-case. (#49)
    Note: this only does validation currently, not transforms. I'll add that
    in the next PR (with an example to demo).

--- julee_example/use_cases/__init__.py
+++ julee_example/use_cases/__init__.py
@@ -5,3 +5,11 @@ This package contains use case classes that orchestrate business logic
 for the Capture, Extract, Assemble, Publish workflow while remaining
 framework-agnostic following Clean Architecture principles.
 """
+
+from .extract_assemble_data import ExtractAssembleDataUseCase
+from .validate_document import ValidateDocumentUseCase
+
+__all__ = [
+    "ExtractAssembleDataUseCase",
+    "ValidateDocumentUseCase",
+]
